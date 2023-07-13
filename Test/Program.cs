a679254eaab8021e920d28b2aa93260a30bd9bf1
Mishany52 <kalintsev309206@gmail.com>
2023-07-13 16:23:01 +0300

    Не закончил решать

---
+++ Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        Boolean end = true;
+        string res;
+        for (int i = 0; i < strs[0].Length; i++)
+        {
+            string curr = Convert.ToString(strs[0][i]);
+            string temp = res + curr;
+
+            while(end){
+                if(strs)
+            }
+        }
+}
