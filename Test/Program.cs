1f7996e06930372338aa42ed4a5a553f0492c418
Mishany52 <kalintsev309206@gmail.com>
2023-07-17 14:13:04 +0300

    Сам не дорешал, а разобрался и взял решение NeetCode

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,15 +1,31 @@
-﻿using System
-public class Solution {
-    public string LongestCommonPrefix(string[] strs) {
-        Boolean end = true;
-        string res;
+﻿using System;
+public class Solution
+{
+
+    public string LongestCommonPrefix(string[] strs)
+    {
+        string res = "";
+
         for (int i = 0; i < strs[0].Length; i++)
         {
-            string curr = Convert.ToString(strs[0][i]);
-            string temp = res + curr;
-
-            while(end){
-                if(strs)
+            foreach (string s in strs)
+            {
+                if(i == s.Length || s[i] != strs[0][i]){
+                    return res;
+                }
+                
             }
+            res += strs[0][i];
         }
+        return res;
+
+    }
+
+    public static void Main()
+    {
+        string[] test = new string[3] {"c","acc","ccc"};
+        Solution test1 = new Solution();
+        Console.WriteLine(test1.LongestCommonPrefix(test));
+    }
+
 }
