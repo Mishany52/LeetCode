d9ed0529c355d9f6669b4b497e1f466c35f09a88
Mishany52 <kalintsev309206@gmail.com>
2023-07-19 15:14:32 +0300

    Решил 1523 leetcode

--- Test/Program.cs
+++ Test/Program.cs
@@ -2,56 +2,29 @@
 public class Solution
 {
 
-    public bool IsValid(string s)
+    public int CountOdds(int low, int high)
     {
-        char curr = s[0];
-        char expected = '\0';
-        bool res = false;
-        Stack<char> c = new Stack<char>();
-        if (s.Length % 2 == 1 ){
-            return res;
-        }
-        for (int i = 0; i < s.Length; i++)
+
+        if ((low % 2 == 1) || (high % 2 == 1))
         {
-            if (s[i] == '(' || s[i] == '{' || s[i] == '['){
-                c.Push(s[i]);
-            }
-            else {
-                if(c.Count == 0){
-                    return false;
-                }
-                curr = s[i];
-                if (curr == ')')
-                {
-                    expected = '(';
-                }
-                if (curr == ']')
-                {
-                    expected = '[';
-                }
-                if (curr == '}')
-                {
-                    expected = '{';
-                }
-                if(expected == c.Pop()){
-                    res = true;
-                }
-                else{
-                    return false;
-                }
-            }
+            return (high - low) / 2 + 1;
         }
-        if (c.Count != 0){
-            return false;
+        else if (low % 2 == 1 && high % 2 == 1)
+        {
+            return (high - low) / 2 - 1;
         }
-        return res;
+        else
+        {
+            return (high - low) / 2;
+        }
+
     }
+
 
     public static void Main()
     {
-        string test = new string("[[[]");
         Solution test1 = new Solution();
-        Console.WriteLine(test1.IsValid(test));
+        Console.WriteLine(test1.CountOdds(0, 10));
     }
 
 }
