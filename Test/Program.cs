e769dba146b01a17f7df103974c22fcb5f175fed
Mishany52 <kalintsev309206@gmail.com>
2023-07-20 17:15:12 +0300

    Задача о среднем значении с выч. мин и макс

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,30 +1,18 @@
 ﻿using System;
 public class Solution
 {
-
-    public int CountOdds(int low, int high)
-    {
-
-        if ((low % 2 == 1) || (high % 2 == 1))
-        {
-            return (high - low) / 2 + 1;
-        }
-        else if (low % 2 == 1 && high % 2 == 1)
-        {
-            return (high - low) / 2 - 1;
-        }
-        else
-        {
-            return (high - low) / 2;
-        }
-
+    public double Average(int[] salary) {
+        double res = salary.Aggregate((acc, x) => acc + x);
+        res = res - (salary.Min() + salary.Max()) / (salary.Length - 2);
+        return res;
+    
     }
-
 
     public static void Main()
     {
+        int[] test = new int [] {48000,59000,99000,13000,78000,45000,31000,17000,39000,37000,93000,77000,33000,28000,4000,54000,67000,6000,1000,11000};
         Solution test1 = new Solution();
-        Console.WriteLine(test1.CountOdds(0, 10));
+        Console.WriteLine(test1.Average(test));
     }
 
 }
