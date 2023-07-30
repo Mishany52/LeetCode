eb5053d1aa6c9efad54ef945e6e354a20a6276b7
Mishany52 <kalintsev309206@gmail.com>
2023-07-30 18:39:29 +0300

    The best speed 1822 leetcode

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,18 +1,43 @@
 ﻿using System;
+using System.Numerics;
 public class Solution
 {
-    public double Average(int[] salary) {
-        double res = salary.Aggregate((acc, x) => acc + x);
-        res = res - (salary.Min() + salary.Max()) / (salary.Length - 2);
-        return res;
-    
+    public int ArraySign(int[] nums) {
+        int negative = 0;
+        bool zero = false;
+        foreach (int item in nums)
+        {
+            if (item < 0) {
+                negative++;
+            }
+            else if (item == 0){
+                zero = true;
+            }
+        }
+        if (negative%2 != 0 && !zero)
+        {
+            return -1;
+        }
+        else if (zero){
+            return 0;
+        }
+        else return 1;
     }
-
+    public int signFunc(long product){
+        if(product > 0) return 1;
+        else if (product < 0) return -1;
+        else return 0;
+    }
+    public int Way(int x1, int y1, int x2, int y2)
+    {
+        return Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
+    }
     public static void Main()
     {
-        int[] test = new int [] {48000,59000,99000,13000,78000,45000,31000,17000,39000,37000,93000,77000,33000,28000,4000,54000,67000,6000,1000,11000};
         Solution test1 = new Solution();
-        Console.WriteLine(test1.Average(test));
+        int[] points = new int[] {-1,0,1};
+        Console.WriteLine(test1.ArraySign(points));
+
     }
 
 }
