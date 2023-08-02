737b1917da9e2568cbd3f573dd30b2c5816bb4e2
Mishany52 <kalintsev309206@gmail.com>
2023-08-02 19:46:06 +0300

    Не дорешал размещение без повторений

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,42 +1,77 @@
 ﻿using System;
 using System.Numerics;
+
 public class Solution
 {
-    public int ArraySign(int[] nums) {
-        int negative = 0;
-        bool zero = false;
-        foreach (int item in nums)
-        {
-            if (item < 0) {
-                negative++;
-            }
-            else if (item == 0){
-                zero = true;
-            }
-        }
-        if (negative%2 != 0 && !zero)
-        {
-            return -1;
-        }
-        else if (zero){
-            return 0;
-        }
-        else return 1;
+    public int LargestPerimeter(int[] nums) {
+        int k = 3;
+        // var test = returnFirstK(nums,k);
+        // for (int i = k; i < nums.Length; i++)
+        // {
+            // if(nums[k-1] < nums[i]){
+            //     break;
+            // }
+        // }
+        reverse(nums,k-1);
+        return 0;
     }
-    public int signFunc(long product){
-        if(product > 0) return 1;
-        else if (product < 0) return -1;
-        else return 0;
+
+    public void swap(int[] sequince, int j, int i){
+        (sequince[i], sequince[j]) = (sequince[j], sequince[i]);
+
     }
-    public int Way(int x1, int y1, int x2, int y2)
-    {
-        return Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
+    public int[] k_permution_of_n(int[] nums, int k){
+        int len = nums.Length;
+        int i = k;
+        for (; i < len && nums[i] < nums[k - 1]; i++)
+        {
+        }
+        
+        if(i < len){
+            swap(nums, k-1, i);
+            
+        }
+        else{
+            reverse(nums,k-1);
+            int j = k - 2;
+            for (;j > 0 && nums[j] > nums[j+1]; j--)
+            { 
+            }
+            if(j<0) 
+                return new int[0];
+            else{
+                i = len - 1;
+                for (; i > j; )
+                {
+                    if (nums[i] > nums[j])
+                        break;
+                    i--;
+                }
+                swap(nums, i, j);
+                reverse(nums, j);
+            }
+        }
+        return nums.Take(k).ToArray();
+    }
+    public void reverse(int[] sequince, int index){
+        var shift = index + 1;
+        int len = sequince.Length;
+        for (int i = 0; i < (len-shift)/2; i++)
+        {
+            (sequince[shift+i], sequince[len-1-i]) = (sequince[len-1-i], sequince[shift+i]);
+        }
     }
     public static void Main()
     {
         Solution test1 = new Solution();
-        int[] points = new int[] {-1,0,1};
-        Console.WriteLine(test1.ArraySign(points));
+        int[] n = new[]{0,1,2,3,4};
+        int[] nums = n.Take(3).ToArray();
+        for (int i = 0;  n.Length!=0 ; i++)
+        {
+            Console.WriteLine(nums);
+            nums = test1.k_permution_of_n(n,3);
+            
+        }
 
     }
 
